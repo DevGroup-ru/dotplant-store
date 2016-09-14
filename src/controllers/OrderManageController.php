21183451fac145f7bb700ec349df7fec246bbf93
fps01 <fps.06@mail.ru>
2016-09-14 14:20:26 +0300

    you ncan not delete an order

--- src/controllers/OrderManageController.php
+++ src/controllers/OrderManageController.php
@@ -98,7 +98,7 @@ class OrderManageController extends Controller
      */
     public function actionDelete($id)
     {
-        $this->findModel($id)->delete();
+//        $this->findModel($id)->delete();
         return $this->redirect(['index']);
     }
 
