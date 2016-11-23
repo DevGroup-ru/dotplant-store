d3f14cb47cd13bfa1fabfa284c9659e532e4c2f3
fps01 <fps.06@mail.ru>
2016-11-23 15:27:30 +0300

    original_quantity migration fix

--- src/migrations/m161108_093450_add_column_original_quantity_in_order_items.php
+++ src/migrations/m161108_093450_add_column_original_quantity_in_order_items.php
@@ -1,36 +1,17 @@
 <?php
 
+use DotPlant\Store\models\order\OrderItem;
 use yii\db\Migration;
 
 class m161108_093450_add_column_original_quantity_in_order_items extends Migration
 {
     public function up()
     {
-        $this->addColumn(
-            \DotPlant\Store\models\order\OrderItem::tableName(),
-            "original_quantity",
-            "integer"
-        );
+        $this->addColumn(OrderItem::tableName(), 'original_quantity', $this->double());
     }
 
     public function down()
     {
-        $this->dropColumn(
-            \DotPlant\Store\models\order\OrderItem::tableName(),
-            "original_quantity"
-        );
-
-        return true;
+        $this->dropColumn(OrderItem::tableName(), 'original_quantity');
     }
-
-    /*
-    // Use safeUp/safeDown to run migration code within a transaction
-    public function safeUp()
-    {
-    }
-
-    public function safeDown()
-    {
-    }
-    */
 }
